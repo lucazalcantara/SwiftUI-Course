7a492b3392ce3e477e9ebc645086386f5e0d4034
Lucas Alcantara <lucasprestesmatos@hotmail.com>
2025-06-07 05:21:08 -0300

    First commit, project created

---
+++ Guess The Flag/Guess The Flag/Guess_The_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_FlagApp.swift
+//  Guess The Flag
+//
+//  Created by Lucas  Alcantara  on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
