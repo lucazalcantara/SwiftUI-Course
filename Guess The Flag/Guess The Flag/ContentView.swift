f4c93a65c9a10d86b4e0e47a989b376811c17e62
Lucas Alcantara <lucasprestesmatos@hotmail.com>
2025-06-08 06:03:43 -0300

    Testing Gradient effects

--- Guess The Flag/Guess The Flag/ContentView.swift
+++ Guess The Flag/Guess The Flag/ContentView.swift
@@ -9,13 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        LinearGradient(gradient: Gradient(colors: [.black, .mint]), startPoint: .top, endPoint: .bottom)
+            .ignoresSafeArea(edges: .all)
     }
 }
 
