69edef093465d9c3741fd26d1d6f57754a5a730e
Lucas Alcantara <lucasprestesmatos@hotmail.com>
2025-09-11 03:51:03 -0300

    Started: iExpense project

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Lucas  Alcantara  on 11/09/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
