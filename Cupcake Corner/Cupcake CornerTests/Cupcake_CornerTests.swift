746fac1464e0046cb992724a440537f2d682362e
Lucas Alcantara <lucasprestesmatos@hotmail.com>
2025-11-21 23:11:42 -0300

    Cupcake Corner project created

---
+++ Cupcake Corner/Cupcake CornerTests/Cupcake_CornerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerTests.swift
+//  Cupcake CornerTests
+//
+//  Created by Lucas  Alcantara  on 21/11/25.
+//
+
+import Testing
+@testable import Cupcake_Corner
+
+struct Cupcake_CornerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
